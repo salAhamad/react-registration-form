23684cdf2502691bb38243dec9674adb4910ad2b
Salman Ahmad <86913182+salAhamad@users.noreply.github.com>
2021-09-06 18:19:16 +0400

    form first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  // Technical Thapa youtube source
+
+  const [formInput, setformInput] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: ""
+  });
+
+  const inputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setformInput({...formInput, [name]: value})
+  }
+  
+  const [records, setRecords] = useState([]);
+
+  const formData = (e) => {
+    e.preventDefault();
+    const newRecord = {...formInput, id: new Date().getTime().toString() }
+    setRecords([newRecord, ...records])
+    console.log(records);
+    setformInput({name: "", email: "", phone: "", password: ""});
+    
+  }
+
+  return <section className="mainSection bg-dark p-5 d-flex justify-content-center align-items-center">
+      <form action="" onSubmit={(e) => formData(e)} className="formContainer">
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Full Name</label>
+          <input type="text" name="name" className="form-control" placeholder="Enter Full Name" value={formInput.name} onChange={(e)=> inputHandler(e)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Email address</label>
+          <input type="email" name="email" className="form-control" placeholder="Enter Email ID" value={formInput.email} onChange={(e)=> inputHandler(e)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Phone Number</label>
+          <input type="number" name="phone" className="form-control" placeholder="Enter Number" value={formInput.phone} onChange={(e)=> inputHandler(e)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Password</label>
+          <input type="password" name="password" className="form-control" placeholder="Enter Password" value={formInput.password} onChange={(e)=> inputHandler(e)} />
+        </div>
+        <button className="btn btn-primary">Register Now</button>
+      </form>
+  </section>
 }
 
 export default App;
